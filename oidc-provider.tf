8baa0bb242bb2641fdab2721942f8d2db334685b
keengwatanabe <mckeenweb@gmail.com>
2025-03-31 21:15:52 +0800

    r

--- oidc-provider.tf
+++ oidc-provider.tf
@@ -1,6 +1,6 @@
 # oidc-provider.tf (run once per AWS account)
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
+resource "aws_iam_openid_connect_provider" "oidc-github" {
+  url             = "arn:aws:iam::255945442255:oidc-provider/token.actions.githubusercontent.com" # https://token.actions.githubusercontent.com
   client_id_list  = ["sts.amazonaws.com"] 
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
