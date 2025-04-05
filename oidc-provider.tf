37535ab414bc2e3306858e4740293729fca069f1
KeenGWatanabe <mckeenweb@gmail.com>
2025-04-05 21:40:47 +0800

    push5

--- oidc-provider.tf
+++ oidc-provider.tf
@@ -10,17 +10,17 @@ resource "aws_iam_role" "github_actions" {
   name = "github-actions-role"
   
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow",
-      Principal = { Federated = aws_iam_openid_connect_provider.github.arn },
-      Action = "sts:AssumeRoleWithWebIdentity",
-      Condition = {
-        StringEquals = { 
-          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com" 
+    "Version": "2012-10-17",
+    "Statement": [{
+      "Effect": "Allow",
+      "Principal": { "Federated": "arn:aws:iam::255945442255:role/github-actions-role" }, # aws_iam_openid_connect_provider.github.arn
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": { 
+          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com" 
         },
-        StringLike = { 
-          "token.actions.githubusercontent.com:sub" = "repo:KeenGWatanabe/odic:ref:refs/heads/main" 
+        "StringLike": { 
+          "token.actions.githubusercontent.com:sub": "repo:KeenGWatanabe/odic:ref:refs/heads/main" 
         }
       }
     }]
